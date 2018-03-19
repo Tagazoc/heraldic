446ab7150b3d93b12fcb89a4529619f3e31e6087
Tagazoc <tagazoc@protonmail.com>
2018-03-19 23:21:37 +0100

    Index refreshing after document store or deletion, to avoid empty GET less than 1 second after.

--- src/store/model_storer.py
+++ src/store/model_storer.py
@@ -8,6 +8,7 @@ from src.models.document_model import DocumentModel, OldDocumentModel
 from src.store.elastic import es
 from elasticsearch.exceptions import NotFoundError
 from src.heraldic_exceptions import DocumentNotChangedException
+from typing import List
 
 
 def store(dm: DocumentModel, doc_id=None):
@@ -21,16 +22,19 @@ def store(dm: DocumentModel, doc_id=None):
 
     res = es.index('docs', 'doc', body, id=doc_id)
     doc_id = res['_id']
+    es.indices.refresh(index='docs')
 
     if dm.has_errors:
         errors_body = dm.render_errors_for_store()
 
         es.index('errors', 'doc_errors', errors_body, id=doc_id)
+        es.indices.refresh(index='errors')
 
     if dm.has_suggestions:
         suggestions_body = dm.render_suggestions_for_store()
 
         es.index('suggestions', 'doc', suggestions_body, id=doc_id)
+        es.indices.refresh(index='suggestions')
 
     return doc_id
 
@@ -48,6 +52,7 @@ def update(dm: DocumentModel, om: OldDocumentModel):
         if dm.has_suggestions:
             suggestions_body = dm.render_suggestions_for_store()
             es.index('suggestions', 'doc', suggestions_body, id=dm.id.value)
+            es.indices.refresh(index='suggestions')
         else:
             try:
                 es.delete('suggestions', 'doc', dm.id.value)
@@ -57,6 +62,7 @@ def update(dm: DocumentModel, om: OldDocumentModel):
         if dm.has_errors:
             errors_body = dm.render_errors_for_store()
             es.index('errors', 'doc_errors', errors_body, id=dm.id.value)
+            es.indices.refresh(index='errors')
         else:
             try:
                 es.delete('errors', 'doc_errors', dm.id.value)
@@ -68,5 +74,24 @@ def update(dm: DocumentModel, om: OldDocumentModel):
 
         om_body = om.render_for_store()
         es.index('docs_history', 'doc', om_body)
+        es.indices.refresh(index='docs_history')
     else:
         raise DocumentNotChangedException
+
+
+def delete(dm: DocumentModel, old_models: List[DocumentModel]) -> None:
+    es.delete('docs', id=dm.id, doc_type='doc')
+    es.indices.refresh(index='docs')
+
+    if dm.has_suggestions:
+        es.delete('suggestions', id=dm.id, doc_type='doc')
+        es.indices.refresh(index='suggestions')
+
+    if dm.has_errors:
+        es.delete('errors', id=dm.id, doc_type='doc_errors')
+        es.indices.refresh(index='errors')
+
+    for old_dm in old_models:
+        es.delete('docs_history', id=old_dm.id, doc_type='doc')
+
+    es.indices.refresh(index='docs_history')
