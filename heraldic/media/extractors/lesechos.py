8a34baf513e946749500fb541b957d3aa1bf8fc6
Tagazoc <tagazoc@protonmail.com>
2018-11-03 22:50:46 +0100

    Additional HTML formatting for Les Echos

--- heraldic/media/extractors/lesechos.py
+++ heraldic/media/extractors/lesechos.py
@@ -17,7 +17,11 @@ class LesEchos(GenericMedia):
     display_name = 'Les Echos'
 
     def _extract_body(self):
-        content_div = copy(self.html_soup).find('div', attrs={'class': 'content-article'})
+        soup_copy = copy(self.html_soup)
+        content_div = soup_copy.find('div', attrs={'class': 'content-article'})
+        if content_div is None:
+            content_div = soup_copy.find('div', attrs={'class': 'contenu_article'})
+
         [div.decompose() for div in content_div.find_all('div', attrs={'class': 'block-alireaussi'})]
         [div.decompose() for div in content_div.find_all('div', attrs={'class': 'encadre-lireaussi'})]
         [div.decompose() for div in content_div.find_all('div', attrs={'class': 'signature-article'})]
@@ -28,9 +32,14 @@ class LesEchos(GenericMedia):
 
     def _extract_href_sources(self):
         html_as = self._body_tag.find_all('a')
+        # Vernimmen definitions are not real articles
+        html_as = self._exclude_hrefs_by_regex(html_as, r'/finance-marches/vernimmen/')
 
         return html_as
 
     def _extract_category(self):
-        text = self.html_soup.find('ul', attrs={'class': 'breadcrumbs'}).find_all('span')[-1].text
+        try:
+            text = self.html_soup.find('ul', attrs={'class': 'breadcrumbs'}).find_all('span')[-1].text
+        except AttributeError:
+            text = self.html_soup.find('div', attrs={'id': 'ariane'}).find_all('a')[-1].text
         return text
