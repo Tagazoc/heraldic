60d08ef4199ff785b8f8a790ee9ab93a409da311
Tagazoc <tagazoc@protonmail.com>
2018-11-09 22:16:11 +0100

    Full backup range stops at last midnight

--- scripts/backup_indices_full.sh
+++ scripts/backup_indices_full.sh
@@ -2,8 +2,8 @@
 
 i=docs
 elasticdump   --input=http://localhost:9200/$i   --output=$   | gzip > /var/backup/$i_words_full.json.gz
-elasticdump   --input=http://localhost:9200/$i   --output=$   --searchBody '{"_source": { "excludes": [ "words" ] }}' | gzip > /var/backup/$i_full.json.gz
+elasticdump   --input=http://localhost:9200/$i   --output=$   --searchBody '{"query": { "range" : { "gather_time" : { "lt" :  "now/d" } } }, "_source": { "excludes": [ "words" ] }}' | gzip > /var/backup/$i_full.json.gz
 for i in docs_history errors suggestions feeds; do
-    elasticdump   --input=http://localhost:9200/$i   --output=$ | gzip > /var/backup/$i_full.json.gz
+    elasticdump   --input=http://localhost:9200/$i --output=$ --searchBody '{"query": { "range" : { "gather_time" : { "lt" :  "now/d" } } }}'| gzip > /var/backup/$i_full.json.gz
 done
 
