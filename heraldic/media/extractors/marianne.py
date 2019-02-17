e207a47a2712ad0dd1ece92c5fb8c3617bb0d633
Tagazoc <tagazoc@protonmail.com>
2019-02-17 20:19:30 +0100

    Marianne parser update, with side links extraction and CSS selector usage

--- heraldic/media/extractors/marianne.py
+++ heraldic/media/extractors/marianne.py
@@ -21,9 +21,17 @@ class MarianneExtractor(GenericMediaExtractor):
     """
     Class used for extracting items from french media "Marianne".
     """
+    test_urls = ['https://www.marianne.net/economie/commercants-artisants-petits-patrons-m-le-president'
+                 '-entendez-le-premier-employeur-de-france',
+                 'https://www.marianne.net/politique/brexit-valerie-pecresse-veut-que-les-britanniques'
+                 '-comprennent-leur-douleur',
+                 'https://www.marianne.net/politique/europeennes-gilet-jaune-ingrid-levavasseur-abandonne'
+                 '-liste-ric']
+
     def _extract_body(self):
-        content_div = copy(self.html_soup).find('div', attrs={'class': 'chapo_body_wrapper'})
-        [div.decompose() for div in content_div.find_all('div', attrs={'class': 'read-more-wysiwyg'})]
+        content_div = self.html_soup.select_one('div.chapo_body_wrapper')
+        side_links = [div.extract().select('a') for div in content_div.select('div.read-more-wysiwyg')]
+        self._side_links.extend([a for div_a in side_links for a in div_a])
         return content_div
 
     def _extract_href_sources(self):
@@ -32,8 +40,12 @@ class MarianneExtractor(GenericMediaExtractor):
         return html_as
 
     def _extract_category(self):
-        text = self.html_soup.find('div', attrs={'class': 'field-name-field-rubriques'}).find_all('a')[-1].text
+        text = self.html_soup.select('div.field-name-field-rubriques a')[-1].text
         return text
 
     def _extract_subscribers_only(self):
-        return self.html_soup.find('div', attrs={'class': 'article-subscription'}) is not None
+        return self.html_soup.select_one('div.article-subscription') is not None
+
+    def _extract_side_links(self):
+        self._side_links.extend(self.html_soup.select('ul.list-article-aside a'))
+        return self._side_links
