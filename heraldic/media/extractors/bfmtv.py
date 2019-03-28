fbd69e65e0fc400d384dbca0153cec7e8680ba85
Tagazoc <tagazoc@protonmail.com>
2019-03-29 00:08:48 +0100

    New extractor for BFMTV videos

--- heraldic/media/extractors/bfmtv.py
+++ heraldic/media/extractors/bfmtv.py
@@ -24,6 +24,7 @@ class BfmTvExtractor(GenericMediaExtractor):
     """
     test_urls = ['https://www.bfmtv.com/politique/hulot-de-retour-en-forme-sur-la-scene-mediatique-apres-'
                  '3-mois-de-silence-1571892.html']
+    default_extractor = True
 
     def _extract_body(self):
         return self.html_soup.find('div', attrs={'itemprop': 'articleBody'})
@@ -41,3 +42,26 @@ class BfmTvExtractor(GenericMediaExtractor):
 
     def _extract_side_links(self):
         return self.html_soup.select('ul.related-article a')
+
+
+class BfmTvVideoExtractor(GenericMediaExtractor):
+    test_urls = ['https://www.bfmtv.com/mediaplayer/video/pour-razzy-hamadi-richard-ferrand-persiste-dans-'
+                 'l-erreur-en-refusant-de-demissionner-950369.html']
+    _document_type = 'video'
+    default_extractor = False
+
+    def _extract_body(self):
+        content_div = self.html_soup.find('h2', attrs={'itemprop': 'description'})
+        try:
+            content_div.find(string="BFMTV, 1ère chaine d'information en continu de France").parent.decompose()
+        except AttributeError:
+            pass
+        return content_div
+
+    def _extract_doc_publication_time(self):
+        return self.html_soup.find('meta', attrs={
+            'property': "og:video:release_date"
+        }).get('content')
+
+    def _check_extraction(self):
+        return self.html_soup.select_one('#page-video') is not None
