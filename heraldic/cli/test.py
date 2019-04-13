c738bab37ee95b5ba363d72b57a5abf0b24995f4
Tagazoc <tagazoc@protonmail.com>
2019-04-13 18:54:14 +0200

    Test command bug fix

--- heraldic/cli/test.py
+++ heraldic/cli/test.py
@@ -9,10 +9,10 @@ import itertools
 
 def test(args):
     if args.media:
-        urls = itertools.chain(*[extractor.test_urls for extractor in known_media[args.media].get_extractors()])
+        urls = list(itertools.chain(*[extractor.test_urls for extractor in known_media[args.media].get_extractors()]))
     else:
-        urls = itertools.chain(*[extractor.test_urls for media_class in known_media.media_classes for extractor in
-                                 media_class.get_extractors()])
+        urls = list(itertools.chain(*[extractor.test_urls for media_class in known_media.media_classes for extractor in
+                                 media_class.get_extractors()]))
 
     url_list = UrlList(urls)
     url_list.harvest(max_depth=0, update_inplace=True, raise_on_optional=True, dump_result=True)
