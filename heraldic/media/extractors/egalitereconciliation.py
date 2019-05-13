d3f2b7fe7589543182db533513392e17a4708bd2
Tagazoc <tagazoc@protonmail.com>
2019-05-13 08:12:13 +0200

    Extractor for french website Egalité & réconciliation

---
+++ heraldic/media/extractors/egalitereconciliation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ website extractor implementation.
+"""
+
+from heraldic.media.generic_media import GenericMedia, GenericMediaExtractor
+import re
+
+
+class EgaliteReconciliation(GenericMedia):
+    """
+    Class used for media "Egalité & Réconciliation".
+    """
+    supported_domains = ['www.egaliteetreconciliation.fr']
+    id = 'egalite_reconciliation'
+    display_name = 'Egalité & réconciliation'
+
+
+class EgaliteReconciliationExtractor(GenericMediaExtractor):
+    """
+    Class used for extracting items from media "Egalité & Réconciliation".
+    """
+    test_urls = ['https://www.egaliteetreconciliation.fr/Le-judeo-christianisme-arme-de-destruction-massive-du-catholicisme-romain-52567.html']
+
+    def _check_extraction(self):
+        return True
+        return self.html_soup.select_one('div.entry-content') is not None
+
+    def _extract_body(self):
+        content_div = self.html_soup.select_one('div.entry-content')
+        notes_div = self.html_soup.select_one('div.notes')
+        content_div.insert(0, notes_div)
+        return content_div
+
+    def _extract_title(self):
+        return self.html_soup.select_one('h1.entry-title').text.strip()
+
+    def _extract_doc_publication_time(self):
+        return self.html_soup.select_one('abbr.published').get('title')
+
+    def _extract_href_sources(self):
+        html_as = self._body_tag.find_all('a')
+
+        return html_as
