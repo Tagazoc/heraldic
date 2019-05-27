10260363b56c891e208e2a13e7ed0347d9412487
Tagazoc <tagazoc@protonmail.com>
2019-05-27 23:44:40 +0200

    Forgot to modify gather CLI file following harvest modifications

--- heraldic/cli/gather.py
+++ heraldic/cli/gather.py
@@ -10,15 +10,15 @@ from heraldic.gathering.feeds import UrlFile, UrlList
 def gather(args):
     url_list = None
     if args.url:
-        url_list = UrlList(args.url)
-        url_list.harvest(max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test,
-                         dump_result=True)
+        url_list = UrlList(args.url, max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test,
+                           dump_result=True)
+        url_list.harvest()
     elif args.file:
-        url_list = UrlFile(args.file)
-        url_list.harvest(max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test,
-                         dump_result=True)
+        url_list = UrlFile(args.file, max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test,
+                           dump_result=True)
+        url_list.harvest()
     else:
         for line in fileinput.input():
-            url_list = UrlList([line])
-            url_list.harvest(max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test, dump_result=True)
+            url_list = UrlList([line], max_depth=args.depth, update_inplace=args.override, raise_on_optional=args.test, dump_result=True)
+            url_list.harvest()
 
